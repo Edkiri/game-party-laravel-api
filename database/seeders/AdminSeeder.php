ce0e5ccf4ff79572b7de0a0f09017b3a97a18915
Eduardo Kiriakos <eduardokiriakos@gmail.com>
2023-07-12 17:04:48 +0200

    feat: seed admin user if not exists

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class AdminSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $admin = DB::table('users')->where('id', 1)->first();
+
+        if (!$admin) {
+            DB::table('users')->insert([
+                'username' => "eduardok",
+                'steam_username' => "",
+                'email' => "edu@mail.com",
+                'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+                'role_id' => 1,
+                'email_verified_at' => now(),
+                'remember_token' => Str::random(10),
+            ]);
+        }
+
+        var_dump($admin);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -10,5 +10,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([RoleSeeder::class]);
+        $this->call([AdminSeeder::class]);
+
+        User::factory(10)->create();
     }
 }
